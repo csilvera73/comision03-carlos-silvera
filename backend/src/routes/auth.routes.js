28482b0b479e33d45f3b86af9d96f46653eb48b7
Carlos <your-email-address>
2023-11-28 20:17:31 -0300

    Se agregaron validaciones, correciones menores

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,17 +3,24 @@
 import { Router } from "express"
 
 // importamos los controllers de auth
-import {register, login, logout, profile} from "../controllers/auth.controller.js"
+import {
+    register, 
+    login, 
+    logout, 
+    profile
+} from "../controllers/auth.controller.js"
 
 import {authRequired} from "../middlewares/validateToken.js"
+
+import {validateLogin, validateRegister, handleErrorValidations} from "../middlewares/validateAttibute.js"
 
 export const routes = Router ()
 
 // Rutas registro de Usuario
-routes.post("/register", register)
+routes.post("/register", validateRegister, handleErrorValidations,register)
 
 // Rutas Login de Usuario
-routes.post("/login",login )
+routes.post("/login",validateRegister, handleErrorValidations,login )
 
 //Rutas para el Logout
 routes.post("/logout", logout)
